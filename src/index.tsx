38b01bedb5fa6f09770c3348b6c6ffc3fc9c9e80
Henrique Bissoli <emp.shad@gmail.com>
2020-07-20 20:38:46 -0300

    React Hooks + Component Type

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import Map from "./components/map";
 
-const App = () => <Map />
+const App : React.FC = () => {
+
+  return <Map />;
+}
 
 render(<App />, document.getElementById('root'));
